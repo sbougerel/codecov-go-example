e24ccffd65619bce502011d86b9ce862e239f41b
Sylvain Bougerel <sylvain.bougerel@grabtaxi.com>
2017-06-19 15:36:07 +0800

    Added comments and empty lines while trying -X fix feature

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func partiallyCovered(a bool, b bool) bool {
 	if !a && b {
+		// This is a comment, and below an intentional empty line
+
 		return false
 	}
 
 	if !b {
+		// This is a comment, and below an intentional empty line
+
 		return false
 	}
 
